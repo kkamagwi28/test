fd400af0b68ef4c5b534995353c7e56907bf3599
Ekaterina Premudraya <smolyaevaekaterina@gmail.com>
2021-09-20 16:26:57 +0600

    checked for mistakes

--- scrypt/scrypt.py
+++ scrypt/scrypt.py
@@ -3,6 +3,7 @@ import re
 import shutil
 import pathlib
 import requests
+import subprocess
 
 from django.conf import settings
 
@@ -26,7 +27,6 @@ class GitCloner:
 
     def if_branch(self, branch):
         if branch in self.branches:
-            print('This branch exists')
             return True
         else:
             return False
@@ -49,13 +49,16 @@ class GitCloner:
             pathlib.Path(f'{repo_dir}').mkdir(parents=True, exist_ok=True)
             # clone repo
             os.chdir(repo_dir)
-            os.popen(f'git clone {source_repo}')
+            out = subprocess.getstatusoutput(f'git clone {source_repo}')
             os.system(f'git remote add s{id} {source_repo}')
-            print('You repository is cloned')
-            # send to socket
 
-            os.chdir(settings.BASE_DIR)
-            self.clone_info = 'Your repository has been cloned'
+            if out[0] == 0:
+                self.clone_info = 'Your repository has been cloned'
+            else:
+                self.clone_info = f"""You have some mistake\n Check repository info\n 
+                                                    If repository exists check branch name in {self.branches}\n
+                                                    If everything is ok contact system administrator to check
+                                                    access level"""
         except Exception:
             self.clone_info = f"""You have some mistake\n Check repository info\n 
                                     If repository exists check branch name in {self.branches}\n
@@ -65,28 +68,29 @@ class GitCloner:
 
     def push_repo(self, source_repo, qwery, id, repo_dir, source_branch, destination_repo, destination_branch):
         try:
-            branches = os.popen('pwd')
-            current_folder = branches.readlines()[0]
-            print(f'Your current folder is {current_folder}')
-
-            print(f'repos/{repo_dir}')
             if os.path.isdir(f'repos/{repo_dir}'):
                 # if branch exists, if no switch branch
                 if self.if_branch(source_branch):
                     # if everything is ok push repo
                     os.chdir(f'repos/{repo_dir}')
-                    os.system(f'git checkout {source_branch}')
-                    os.system(f'git remote add d{id} {destination_repo}')
+                    out1 = subprocess.getstatusoutput(f'git checkout {source_branch}')
+                    out2 = subprocess.getstatusoutput(f'git remote add d{id} {destination_repo}')
                     os.system(f'git fetch d{id}')
                     os.system(f'git branch -M {destination_branch}')
-                    os.system(f'git push d{id} {destination_branch}')
+                    out3 = subprocess.getstatusoutput(f'git push d{id} {destination_branch}')
+                    if out1[0] == 0 and out2[0] == 0 and out3[0] == 0:
+                        self.push_info = 'Repository has been pushed'
+                    else:
+                        self.push_info = f"""You have some mistake\n Check repository info\n 
+                                            If repository exists check branch name in {self.branches}\n
+                                            If everything is ok contact system administrator to check
+                                            access level"""
                 else:
-                    self.switch_branch(source_branch, 's', id, repo_dir)
+                    self.push_info = "Please, check source branch name and edit info"
             # if no clone repo
             elif not os.path.isdir(repo_dir):
                 self.clone_repo(id, qwery, repo_dir, source_repo, source_branch, destination_repo, destination_branch)
                 # self.push_repo(source_repo, qwery, id, repo_dir, source_branch, destination_repo, destination_branch)
-            print('Customer repository has been updated')
 
             self.push_info = 'Repository has been pushed'
         except:
@@ -104,20 +108,27 @@ class GitCloner:
                 if self.if_branch():
                     # if everything is ok push repo
                     os.chdir(repo_dir)
-                    os.system(f'git pull s{id} {source_branch}')
+                    out1 = subprocess.getstatusoutput(f'git pull s{id} {source_branch}')
                     os.system(f'git fetch s{id}')
                     os.system(f'git remote add d{id} {destination_repo_adress}')
                     os.system(f'git checkout --track s{id}/{source_branch}')
-                    os.system(
+                    out2 = subprocess.getstatusoutput(
                         f'git push d{id} refs/remotes/s{id}/{source_branch}:refs/remotes/d{id}/{destination_branch}')
+                    if out1[0] == 0 and out2[0] == 0:
+                        self.update_info = 'Repository has been updated'
+                    else:
+                        self.update_info = f"""You have some mistake\n Check repository info\n 
+                                                If repository exists check branch name in {self.branches}\n
+                                                If everything is ok contact system administrator to check
+                                                access level"""
                 else:
-                    print('Change branch and send data again')
+                    self.update_info = 'Change branch and send data again'
             # if no clone repo
             elif not os.path.isdir(repo_dir):
                 self.clone_repo(source_repo, qwery, id, repo_dir)
                 self.update_repo(source_repo, qwery, id, repo_dir, source_branch, destination_repo_adress, destination_branch)
 
-            self.update_info = 'Repository has been updated'
+
         except:
             self.update_info = f"""You have some mistake\n Check repository info\n 
                                 If repository exists check branch name in {self.branches}\n
@@ -136,8 +147,8 @@ class GitCloner:
             shutil.rmtree(folder_path)
             print('Project and folder deleted')
         else:
-            # file not found message
-            print("File not found in the directory")
+            # folder not found message
+            print("Folder doesn't exist")
 
         os.chdir(settings.BASE_DIR)
 
